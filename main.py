31982ccda1a931d0987abbbf4181eb631fa8afd1
Adriano Vieira <tsiadriano@gmail.com>
2024-08-21 15:11:50 -0300

    [BUILD] Refact to deploy

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-from typing import Union
-from dotenv import load_dotenv
-from fastapi import FastAPI
-from services.services import Service, get_service
-
-app = FastAPI()
-load_dotenv()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.post("/services/")
-async def service(request: Service):
-    res = get_service(request)
-    return res
+from api.index import app
